0c4529b9878eccbed78d880b102b23fd5be39fd6
brookewebb <kuonisacoolguy@hotmail.com>
2022-02-08 20:02:47 -0500

    initial commit

---
+++ functions_intermediate.py
@@ -0,0 +1,102 @@
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+
+x[1][0] = 15
+print(x)
+
+
+
+for student in range(len(students)):
+    if students[student]['last_name'] == 'Jordan':
+        students[student]['last_name'] = 'Bryant'
+print(students)
+
+sports_directory['soccer'][0] = 'Andres'
+print(sports_directory)
+
+z[0]['x'] = 30
+print(z)
+
+
+
+
+# Come back to above this one
+
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(some_list):
+    for i in some_list:
+        print('first_name - ', i['first_name'] + ', last_name - ', i['last_name'])
+        
+        
+
+
+
+
+iterateDictionary(students) 
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+
+def iterateDictionary2(key_name, some_list):
+    for i in some_list:
+        print(i[key_name])
+
+iterateDictionary2('first_name', students)
+iterateDictionary2('last_name', students)
+
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+def printInfo(some_dict):
+    for i in some_dict:
+        print(len(some_dict[i]), i) 
+        for x in some_dict[i]:
+            print(x)
+        
+
+printInfo(dojo)
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+    
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
+
+
+
